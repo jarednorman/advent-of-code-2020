2d4df95985b49ea4c6ab2d3639faafe1234c8cf1
Jared Norman <jared@super.gd>
2020-11-27 02:39:14 +0000

    Who needs that extra class

--- aoc.rb
+++ aoc.rb
@@ -57,7 +57,7 @@ module AoC
 
         generate_file(
           path: input_path,
-          contents: AoC::Input.fetch(year: year, day: day)
+          contents: input_data
         )
       end
     end
@@ -104,6 +104,16 @@ module AoC
 
     def year_const_string
       "Year#{year}"
+    end
+
+    def input_data
+      uri = URI("https://adventofcode.com/#{year}/day/#{day}/input")
+      http = Net::HTTP.new(uri.host, 443)
+      http.use_ssl = true
+      request = Net::HTTP::Get.new(uri.request_uri)
+      request['Cookie'] = ENV.fetch('AOC_SESSION_COOKIE')
+      response = http.request(request)
+      response.body
     end
   end
 end
