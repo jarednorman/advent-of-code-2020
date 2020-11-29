54cb426563b55ba5281b0e9921f95576dc0335c6
Jared Norman <jared@super.gd>
2020-11-29 00:28:43 +0000

    Generate year 2019 day 7

---
+++ aoc/year2019/day7.rb
@@ -0,0 +1,17 @@
+class AoC::Year2019::Day7
+  def solution
+    0
+  end
+
+  def input
+    @input ||= File.read("aoc/year2019/day7.txt")
+  end
+end
+
+RSpec.describe AoC::Year2019::Day6 do
+  let(:instance) { described_class.new }
+
+  describe "#solution" do
+    subject { instance.solution }
+  end
+end
