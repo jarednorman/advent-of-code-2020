5562ed5159a6f60509dd9bbcf063853f1782819d
Jared Norman <jared@super.gd>
2020-12-01 05:29:13 +0000

    Tests pass for year 2020 day 1

--- aoc/year2020/day1.rb
+++ aoc/year2020/day1.rb
@@ -1,9 +1,9 @@
 module AoC::Year2020::Day1
   class Part1
     def solution(input = real_input)
-      pp(input.split.map(&:to_i).combination(combo_size).find do |x|
+      input.split.map(&:to_i).combination(combo_size).find do |x|
         x.sum == 2020
-      end).inject(&:*)
+      end.inject(&:*)
     end
 
     private
