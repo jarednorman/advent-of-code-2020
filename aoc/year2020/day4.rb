7441655a7f3bb6a8ef889d1d639d559d556e2a2b
Jared Norman <jared@super.gd>
2020-12-04 05:05:15 +0000

    Tests pass for year 2020 day 4

--- aoc/year2020/day4.rb
+++ aoc/year2020/day4.rb
@@ -2,6 +2,19 @@ module AoC::Year2020::Day4
   RSpec.shared_context "sample input" do
     let(:input) {
       <<~INPUT
+      ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+      byr:1937 iyr:2017 cid:147 hgt:183cm
+
+      iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+      hcl:#cfa07d byr:1929
+
+      hcl:#ae17e1 iyr:2013
+      eyr:2024
+      ecl:brn pid:760753108 byr:1931
+      hgt:179cm
+
+      hcl:#cfa07d eyr:2025 pid:166559648
+      iyr:2011 ecl:brn hgt:59in
       INPUT
     }
   end
@@ -31,6 +44,10 @@ module AoC::Year2020::Day4
       subject { instance.solution }
 
       include_context "sample input"
+
+      it "solves the sample input", :pending do
+        expect(subject).to eq 2
+      end
     end
   end
 
