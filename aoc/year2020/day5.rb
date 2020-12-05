fa3d5a2533538daa786201d6bc586356d34b8b95
Jared Norman <jared@super.gd>
2020-12-05 05:03:23 +0000

    Generate year 2020 day 5

---
+++ aoc/year2020/day5.rb
@@ -0,0 +1,53 @@
+module AoC::Year2020::Day5
+  RSpec.shared_context "sample input" do
+    let(:input) {
+      <<~INPUT
+      INPUT
+    }
+  end
+
+  class Part1
+    def initialize(input = real_input)
+      @input = input
+    end
+
+    def solution
+      0
+    end
+
+    private
+
+    attr_reader :input
+
+    def real_input
+      @input ||= File.read("aoc/year2020/day5.txt")
+    end
+  end
+
+  RSpec.describe Part1 do
+    let(:instance) { described_class.new(input) }
+
+    describe "#solution" do
+      subject { instance.solution }
+
+      include_context "sample input"
+    end
+  end
+
+  class Part2 < Part1
+    def solution
+      0
+    end
+  end
+
+  RSpec.describe Part2 do
+    let(:instance) { described_class.new(input) }
+
+    describe "#solution" do
+      subject { instance.solution }
+
+      include_context "sample input"
+    end
+  end
+end
+
