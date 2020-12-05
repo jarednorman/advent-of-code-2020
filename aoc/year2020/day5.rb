fc689b435368dd0d1ce3b5dfaee156fff57de6e1
Jared Norman <jared@super.gd>
2020-12-05 05:08:19 +0000

    Tests pass for year 2020 day 5

--- aoc/year2020/day5.rb
+++ aoc/year2020/day5.rb
@@ -6,6 +6,26 @@ module AoC::Year2020::Day5
     }
   end
 
+  class BoardingPass
+  end
+
+  RSpec.describe BoardingPass do
+    it "works for the examples", :pending do
+      pass = described_class.new("BFFFBBFRRR")
+      expect(pass.row).to eq 70
+      expect(pass.column).to eq 7
+      expect(pass.id).to eq 567
+      pass = described_class.new("FFFBBBFRRR")
+      expect(pass.row).to eq 14
+      expect(pass.column).to eq 7
+      expect(pass.id).to eq 119
+      pass = described_class.new("BBFFBBFRLL")
+      expect(pass.row).to eq 102
+      expect(pass.column).to eq 4
+      expect(pass.id).to eq 820
+    end
+  end
+
   class Part1
     def initialize(input = real_input)
       @input = input
