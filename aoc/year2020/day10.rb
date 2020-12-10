9fefc83aee4971ff3be16476d505a5b2c9ce43b6
Jared Norman <jared@super.gd>
2020-12-10 05:33:54 +0000

    Day ten part two

--- aoc/year2020/day10.rb
+++ aoc/year2020/day10.rb
@@ -1,13 +1,13 @@
 module AoC::Year2020::Day10
   class Part1
     def initialize(input = real_input)
-      @input = input.split("\n").map(&:to_i)
+      @input = input.split("\n").map(&:to_i).sort
     end
 
     def solution
       ones = 0
-      threes = 1
-      ([0] + input).sort.each_cons(2) do |a,b|
+      threes = 0
+      ([0] + input + [laptop]).each_cons(2) do |a,b|
         ones += 1 if b - a == 1
         threes += 1 if b - a == 3
       end
@@ -17,6 +17,10 @@ module AoC::Year2020::Day10
     private
 
     attr_reader :input
+
+    def laptop
+      input.last + 3
+    end
 
     def real_input
       @input ||= File.read("aoc/year2020/day10.txt")
@@ -79,13 +83,79 @@ module AoC::Year2020::Day10
 
   class Part2 < Part1
     def solution
-      0
+      solutions(0)
+    end
+
+    def solutions(current)
+      return 1 if laptop == current
+      @h ||= {}
+
+      @h[current] ||= valid_next_options(current).map do |x| solutions(x) end.sum
+    end
+
+    def valid_next_options(n)
+      @m ||= {}
+      @m[n] ||= chain.select do |x|
+        delta = x - n
+        (1..3).include? delta
+      end
+    end
+
+    def chain
+      @chain ||= input + [laptop]
     end
   end
 
   class Part2Test < Minitest::Test
-    def test_sample_input
-      assert_equal 0, Part2.new(<<~INPUT).solution
+    def test_sample_input_one
+      assert_equal 8, Part2.new(<<~INPUT).solution
+        16
+        10
+        15
+        5
+        1
+        11
+        7
+        19
+        6
+        12
+        4
+      INPUT
+    end
+
+    def test_sample_input_two
+      assert_equal 19208, Part2.new(<<~INPUT).solution
+        28
+        33
+        18
+        42
+        31
+        14
+        46
+        20
+        48
+        47
+        24
+        23
+        49
+        45
+        19
+        38
+        39
+        11
+        1
+        32
+        25
+        35
+        8
+        17
+        7
+        9
+        4
+        2
+        34
+        10
+        3
       INPUT
     end
   end
