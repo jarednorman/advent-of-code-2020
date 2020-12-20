b3504c4c1c2810aac09fc46d46333a7464f9651d
Jared Norman <jared@super.gd>
2020-12-20 14:50:09 -0800

    Day 19 part two

--- aoc/year2020/day19.rb
+++ aoc/year2020/day19.rb
@@ -141,7 +141,7 @@ module AoC::Year2020::Day19
     end
 
     def solution
-      ruleset = RuleSet.new(rules)
+      ruleset = ruleset_class.new(rules)
 
       messages.count { |m| ruleset.match? m }
     end
@@ -149,6 +149,10 @@ module AoC::Year2020::Day19
     private
 
     attr_reader :input
+
+    def ruleset_class
+      RuleSet
+    end
 
     def rules
       input.rules
@@ -182,15 +186,70 @@ module AoC::Year2020::Day19
     end
   end
 
+  class RuleSet2 < RuleSet
+    def initialize(rules)
+      super
+      rules[8] = "(#{to_regex(42)}+)"
+      rules[11] = "(?<re>(#{to_regex(42)})\\g<re>?(#{to_regex(31)}))"
+    end
+  end
+
   class Part2 < Part1
-    def solution
-      0
+    def ruleset_class
+      RuleSet2
     end
   end
 
   class Part2Test < Minitest::Test
     def test_sample_input
-      assert_equal 0, Part2.new(<<~INPUT).solution
+      assert_equal 12, Part2.new(<<~INPUT).solution
+        42: 9 14 | 10 1
+        9: 14 27 | 1 26
+        10: 23 14 | 28 1
+        1: "a"
+        11: 42 31
+        5: 1 14 | 15 1
+        19: 14 1 | 14 14
+        12: 24 14 | 19 1
+        16: 15 1 | 14 14
+        31: 14 17 | 1 13
+        6: 14 14 | 1 14
+        2: 1 24 | 14 4
+        0: 8 11
+        13: 14 3 | 1 12
+        15: 1 | 14
+        17: 14 2 | 1 7
+        23: 25 1 | 22 14
+        28: 16 1
+        4: 1 1
+        20: 14 14 | 1 15
+        3: 5 14 | 16 1
+        27: 1 6 | 14 18
+        14: "b"
+        21: 14 1 | 1 14
+        25: 1 1 | 1 14
+        22: 14 14
+        8: 42
+        26: 14 22 | 1 20
+        18: 15 15
+        7: 14 5 | 1 21
+        24: 14 1
+
+        abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+        bbabbbbaabaabba
+        babbbbaabbbbbabbbbbbaabaaabaaa
+        aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+        bbbbbbbaaaabbbbaaabbabaaa
+        bbbababbbbaaaaaaaabbababaaababaabab
+        ababaaaaaabaaab
+        ababaaaaabbbaba
+        baabbaaaabbaaaababbaababb
+        abbbbabbbbaaaababbbbbbaaaababb
+        aaaaabbaabaaaaababaa
+        aaaabbaaaabbaaa
+        aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+        babaaabbbaaabaababbaabababaaab
+        aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
       INPUT
     end
   end
